72b8ce950358184845b3246a1f09fb5441b70035
LonelyBush <lonely.bush68@gmail.com>
2025-06-16 21:58:57 +0300

    feat: complete auth module and added bcrypto

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import { FavsModule } from './modules/favs/favs.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { FileModule } from './modules/file/file.module';
 import { LoggerModule } from './modules/logger/logger.module';
-import { CustomLogger } from './modules/logger/logger.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -20,10 +22,16 @@ import { CustomLogger } from './modules/logger/logger.service';
     FavsModule,
     LoggerModule,
     FileModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule implements NestModule {
-  constructor(private logger: CustomLogger) {}
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
